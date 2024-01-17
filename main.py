cc3dd19c9eab45830217a3586a0ce14329a5500f
aiki-engine <156732782+aiki-engine@users.noreply.github.com>
2024-01-17 07:43:46 +0700

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+# Insert a chat message container.
+>>> with st.chat_message("user"):
+>>>    st.write("Hello 👋")
+>>>    st.line_chart(np.random.randn(30, 3))
+
+# Display a chat input widget.
+>>> st.chat_input("Say something")
+
+
 st.write ("""
 # Aplikasi Luas Segitiga
 Ini adalah aplikasi menghitung luas segitiga menggunakan Streamlit""")
