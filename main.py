81699d6aa68e50c949a6df7c3d23ff36a31ecaef
aiki-engine <156732782+aiki-engine@users.noreply.github.com>
2024-01-17 07:47:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ st.write ("""
 Ini adalah aplikasi menghitung luas segitiga menggunakan Streamlit""")
 
 
->>> with st.chat_message("user"):
->>>    st.write("Hello 👋")
->>>    st.line_chart(np.random.randn(30, 3))
+with st.chat_message("user"):
+st.write("Hello 👋")
+st.line_chart(np.random.randn(30, 3))
 
-
->>> st.chat_input("Say something")
+st.chat_input("Say something")
